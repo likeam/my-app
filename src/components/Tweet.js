d1a2c1f5b881b2f135a959eb079835c52679eefa
AbdulRehman <likeam99@gmail.com>
2022-07-19 14:33:13 +0500

    Tweet rendering

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export default function Tweet({name, message}) {
+export default function Tweet({name, message, setTweets }) {
+
+    const deleteTweet = ()=>{
+        console.log('Delete It');
+    }
+
     return (
     
         <div className='tweet'>
             <h2>{name}</h2>
             <h3>{message}</h3>
-            <button>Delete</button>
+            <button onClick={deleteTweet}>Delete</button>
             <button>Like</button>
         </div>
-    )
+    );
 }
